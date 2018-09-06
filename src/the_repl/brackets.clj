eb96f731f81c041cdc41003c0229b119780026ff
ertugrulcetin <ertu413@gmail.com>
2018-09-06 16:18:58 +0300

    refactor create-match-brackets-indices-map

--- src/the_repl/brackets.clj
+++ src/the_repl/brackets.clj
@@ -1,24 +1,15 @@
 (ns the-repl.brackets)
 
 
-(def src-indexes (atom nil))
-
 (def bracket-open-close-indices (atom {}))
 
 
-(defn create-src-vec
-  [code]
-  (reset! src-indexes (vec (seq code))))
-
-
 (defn create-match-brackets-indices-map
-  []
-  (let [parens (filter (fn [[e _]]
-                         (#{\( \)} e))
-                       (keep-indexed (fn [i e] [e i]) @src-indexes))
-        p      (partition 2 (partition-by (fn [[e _]] e) parens))
-        m      (apply merge (for [x p]
-                              (into {} (map (fn [[_ i1] [_ i2]]
-                                              [i1 i2]) (first x) (reverse (second x))))))]
-    (reset! bracket-open-close-indices {:open  m
-                                        :close (clojure.set/map-invert m)})))
+  [code]
+  (let [seq-chars (vec (seq code))
+        parens    (filter (fn [[e _]] (#{\( \)} e)) (keep-indexed (fn [i e] [e i]) seq-chars))
+        p         (partition 2 (partition-by (fn [[e _]] e) parens))
+        m         (apply merge (for [x p]
+                                 (into {} (map (fn [[_ i1] [_ i2]]
+                                                 [i1 i2]) (first x) (reverse (second x))))))]
+    (reset! bracket-open-close-indices {:open m :close (clojure.set/map-invert m)})))
